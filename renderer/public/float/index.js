6eb7d3a7cbae4b01a2f491100e8fab2407825c73
Lukas <52617815+LukasHe0908@users.noreply.github.com>
2024-04-03 14:04:10 +0800

    Update index.js

--- renderer/public/float/index.js
+++ renderer/public/float/index.js
@@ -337,7 +337,7 @@ async function start() {
         if (currentTime >= classStartTime && currentTime <= classEndTime) {
           // 正在上的课程
           // classElement.style.backgroundColor = '#F3F6FC';
-          classElement.style.backgroundColor = 'var(--mdui-color-tertiary-container)';
+          classElement.style.backgroundColor = 'var(--mdui-color-tertiary-container-light)';
           temp_scroll_item = classElement;
         } else if (currentTime > classEndTime) {
           // 已经上的课程
