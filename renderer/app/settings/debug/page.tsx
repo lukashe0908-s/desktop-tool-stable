1b04cff3a253f142d0eaf5a9e9254ff94abd44f4
Lukas <52617815+LukasHe0908@users.noreply.github.com>
2024-05-21 20:37:12 +0800

    Update page.tsx

--- renderer/app/settings/debug/page.tsx
+++ renderer/app/settings/debug/page.tsx
@@ -7,7 +7,7 @@ export default function App() {
       <div className='flex gap-5 flex-col'>
         <Card>
           <CardBody className='block'>
-            <span className='font-bold text-xl'>作者：472 秦子元</span>
+            <span className='font-bold text-xl'>作者：章教授</span>
           </CardBody>
         </Card>
         <div className='flex gap-4 flex-wrap'>
